7aae20ca9172c1df84fea7d45cd28a85d4f39fb5
Mia <mia@vidops.io>
2021-01-11 18:13:27 +0000

    transferred funds between customers

--- app/Main.hs
+++ app/Main.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE RecordWildCards #-}
+
 module Main where
 
 import Data.Map (Map)
@@ -28,32 +31,44 @@ main = do
     let c1 = Customer {name = "C1", balance = 100, account = One}
     let c2 = Customer {name = "C2", balance = 100, account = Two} 
     let c3 = Customer {name = "C3", balance = 100, account = Three}
+    let c4 = Customer {name = "C4", balance = 20, account = Four}
+    let c5 = Customer {name = "C5", balance = 20, account = Five}
+    let c6 = Customer {name = "C6", balance = 20, account = Six}
+    let c7 = Customer {name = "C3", balance = 20, account = Seven}
+    let c8 = Customer {name = "C8", balance = 20, account= Eight}
+    let c9 = Customer {name = "C9", balance = 20, account = Nine}
+    let c10 = Customer {name = "C10", balance = 20, account = Ten}
+    putStrLn $ "10 customers created."
 
     b <- newEmptyMVar
-    mapM_  forkIO [customerthreads c1 b box, customerthreads c2 b box, customerthreads c3 b box]
+    putStrLn $ "10 customer threads being created."
+    randomRIO (1,10) >>= \r -> threadDelay (r * 100000)
+    mapM_ forkIO [customerthreads c1 b box, customerthreads c2 b box, customerthreads c3 b box, customerthreads c4 b box, customerthreads c5 b box, customerthreads c6 b box, customerthreads c7 b box, customerthreads c8 b box, customerthreads c9 b box, customerthreads c10 b box]
     putStrLn "Press Return to show the results."
     _ <- getLine
     randomRIO (1,10) >>= \r -> threadDelay (r * 100000)
     c <- takeMVar b 
-    putStrLn $ "The winner is: " ++ (show c)
-    -- run again to find second winner?
-    putStrLn $ " -- putting winner coin back in the box"
+    putStrLn $ "The payee is: " ++ (show c)
+    -- run again to find second winner
+    putStrLn $ " -- putting coin back in the box"
     putMVar b c
-    putStrLn "Press Return to find the second winner."
+    putStrLn "Press Return to find the recipient."
     _ <- getLine
-    --coin2 <- coinFlip
-    --putStrLn $ "Random coin is: " ++ (show coin2)
-    --putStrLn $ " -- creating a new coin box and flipping the coin again"
     box2 <- newMVar coin
-    -- **
     b2 <- newEmptyMVar
     mapM_  forkIO [customerthreads c1 b2 box2, customerthreads c2 b2 box2, customerthreads c3 b2 box2]
     randomRIO (1,10) >>= \r -> threadDelay (r * 100000)
     putStrLn "Press Return to show the results."
     _ <- getLine
     d <- takeMVar b2
-    putStrLn $ "The second winner is: " ++ (show d)
-    
+    putStrLn $ "The recipient is: " ++ (show d) ++ "The payee is: " ++ (show c)
+    (c, d) <- transfer c d 10
+    putStrLn $ "****UPDATED*****The recipient is: " ++ (show d) ++ "The payee is: " ++ (show c)
+
+    {-if (d balance) == (c balance) then
+        putStrLn $ "wow"
+        else 
+            putStrLn "nope"-}
 
 data Coin = Head | Tail deriving (Show, Eq)
 coinFlip :: IO Coin
@@ -61,18 +76,46 @@ coinFlip = do
     r <- randomIO :: IO Bool
     return $ if r then Head else Tail
 
-customerthreads :: Customer -> MVar (String, Customer) -> MVar Coin -> IO ()    
+
+customerthreads :: Customer -> MVar Customer -> MVar Coin -> IO ()    
 customerthreads cust b box = do 
     c1 <- coinFlip
     c2 <- takeMVar box
     putStrLn $ (show cust) ++ " -- got " ++ (show c1)
     if c1 == c2 then do
-        putMVar b ((" We've got a winner: "), cust)
+        putMVar b (cust)
     else do
         putStrLn $ " -- putting coin back in the box "
         putMVar box c2
         randomRIO (1,10) >>= \r -> threadDelay (r * 100000)
         customerthreads cust b box
 
+transfer :: Customer -> Customer -> Int -> IO (Customer, Customer)
+transfer from to amount
+  -- | amount <= 0 = return (from, to)
+  | balance from < amount = return (from, to)
+  | otherwise = return ((from { balance =  ((balance  from) - amount)}),(to { balance  =  ((balance  to) + amount)}))        
 
-        
+-- This function creates threads with the MVar holding two things 
+{-customerthreads :: Customer -> MVar (String, Customer) -> MVar Coin -> IO ()    
+customerthreads cust b box = do 
+    c1 <- coinFlip
+    c2 <- takeMVar box
+    putStrLn $ (show cust) ++ " -- got " ++ (show c1)
+    if c1 == c2 then do
+        putMVar b ((" Details: "), cust)
+    else do
+        putStrLn $ " -- putting coin back in the box "
+        putMVar box c2
+        randomRIO (1,10) >>= \r -> threadDelay (r * 100000)
+        customerthreads cust b box-}
+
+
+-- Next steps:
+-- now we have two customers - maybe put them together in an MVar in main?
+-- or make the MVar with Customer + String just Customer?
+-- then create a function to transfer money between them 
+-- then test running the function        
+
+--Still can't
+-- access Customer data points
