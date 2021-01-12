ee042c022e54deb7d74b9d8c0e9e4bfce177e5ac
Mia <mia@vidops.io>
2021-01-12 11:16:21 +0000

    updated so all forks included

--- app/Main.hs
+++ app/Main.hs
@@ -25,7 +25,7 @@ type Name    = String
 data Account = One | Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten deriving (Show, Eq)
 type Balance = Int
 
-{-
+
 -- 11.01.21 Monday's coinflip with:
 -- Random thread delays + getline to process results
 main :: IO ()
@@ -62,13 +62,14 @@ main = {-forever $-} do
        --_ <- getLine
        box2 <- newMVar coin
        b2 <- newEmptyMVar
-       mapM_  forkIO [customerthreads c1 b2 box2, customerthreads c2 b2 box2, customerthreads c3 b2 box2]
+       mapM_ forkIO [customerthreads c1 b2 box2, customerthreads c2 b2 box2, customerthreads c3 b2 box2, customerthreads c4 b2 box2, customerthreads c5 b2 box2, customerthreads c6 b2 box2, customerthreads c7 b2 box2, customerthreads c8 b2 box2, customerthreads c9 b2 box2, customerthreads c10 b2 box2]
        randomRIO (1,10) >>= \r -> threadDelay (r * 100000)
        putStrLn "Press Return to show the results."
        --_ <- getLine
        d <- takeMVar b2
        amount <- randomN
        putStrLn $ "The recipient is: " ++ (show d) ++ "The payee is: " ++ (show c) ++ " and the random amount is: " ++ (show amount)
+       
        (c, d) <- transfer c d amount
        putStrLn $ "****UPDATED*****The recipient is: " ++ (show d) ++ "The payee is: " ++ (show c)
        
@@ -109,7 +110,7 @@ randomN = do
     r <- randomRIO (10, 50)
     return r
 
--}
+
 
 -- This function creates threads with the MVar holding two things 
 {-customerthreads :: Customer -> MVar (String, Customer) -> MVar Coin -> IO ()    
@@ -140,6 +141,13 @@ customerthreads cust b box = do
     -- run 100 times (?)
         -- print out all the final accounts
 
+
+
+
+
+
+
+{-
 -- ****** TRY to add the second check to the customer thread
 -- Each customer thread should behave as follows: at random time intervals, the thread should select one of the other customers at random, 
 -- and transfer a random amount of money (between £10 and £50)  into their account. 
@@ -163,22 +171,24 @@ main = {-forever $-} do
        let c10 = Customer {name = "C10", balance = 20, account = Ten}
        putStrLn $ "10 customers created."
 
-       b <- newEmptyMVar
+       mcusta <- newEmptyMVar -- (for the payee)
+       mcustb <- newEmptyMVar -- (for the recipient)
+       xcoin <-  newEmptyMVar -- (for the coin)
        putStrLn $ "10 customer threads being created."
        randomRIO (1,10) >>= \r -> threadDelay (r * 100000)
-       mapM_ forkIO [customerthreads c1 b box, customerthreads c2 b box, customerthreads c3 b box, customerthreads c4 b box, customerthreads c5 b box, customerthreads c6 b box, customerthreads c7 b box, customerthreads c8 b box, customerthreads c9 b box, customerthreads c10 b box]
+       mapM_ forkIO [customerthreads c1 mcusta xcoin box mcustb, customerthreads c2 mcusta xcoin box mcustb, customerthreads c3 mcusta xcoin box mcustb, customerthreads c4 mcusta xcoin box mcustb, customerthreads c5 mcusta xcoin box mcustb, customerthreads c6 mcusta xcoin box mcustb, customerthreads c7 mcusta xcoin box mcustb, customerthreads c8 mcusta xcoin box mcustb, customerthreads c9 mcusta xcoin box mcustb, customerthreads c10 mcusta xcoin box mcustb]
        --putStrLn "Press Return to show the results_b."
        --_ <- getLine
        --U randomRIO (1,10) >>= \r -> threadDelay (r * 100000)
-       c <- takeMVar b 
+       --T c <- takeMVar b 
        --h <- newEmptyMVar
-       putStrLn $ "The payee is: " ++ (show c)
+       --T putStrLn $ "The payee is: " ++ (show c)
        ---RU h <- newMVar coin
        --g <- takeMVar h
        -- putStrLn $ "The rec is: " ++ (show g)
     -- run again to find second winner
        --U putStrLn $ " -- putting coin back in the box_b"
-       putMVar b c
+       --T putMVar b c
        --putStrLn "Press Return to find the recipient."
        --U _ <- getLine
        --U box2 <- newMVar coin
@@ -208,30 +218,40 @@ coinFlip = do
     return $ if r then Head else Tail
 
 
-customerthreads :: Customer -> MVar Customer -> MVar Coin -> IO ()    
-customerthreads cust b box = do 
-    c1 <- coinFlip
-    c2 <- takeMVar box
+customerthreads :: Customer -> MVar Customer -> MVar Coin -> MVar Coin -> MVar Customer -> IO ()    
+customerthreads cust mcusta xcoin box mcustb = do 
+    c1 <- coinFlip -- each customer flips their own coin and saves it to c1
+    c2 <- takeMVar box -- then we take an MVar from the main box c2
+    putStrLn $ (show cust) ++ "is playing" -- then we show the customer that is playing
+    putMVar xcoin c1
+    k <- takeMVar xcoin
     putStrLn $ (show cust) ++ " -- got " ++ (show c1)
     if c1 == c2 then do
         putStrLn "Match! Press return"
         _ <- getLine
-        putMVar b (cust)
+        putMVar mcusta (cust)
         putStrLn $ " -- test for next customer -- putting coin back in the box"
-        putMVar box c2
         putStrLn $ " -- checking for the next customer that matched random coin"
+        
+        putMVar box c2
+        randomRIO (1,10) >>= \r -> threadDelay (r * 100000)
+        c1 <- coinFlip
         c2 <- takeMVar box
-        customerthreads cust b box
-        putStrLn $ (show cust) ++ " -- also got! " ++ (show c2)
+        putStrLn $ (show cust) ++ " -- got_b " ++ (show c1)
+        
+        
+        --putMVar box c2
+        --randomRIO (1,10) >>= \r -> threadDelay (r * 100000)
+        --customerthreads cust b x box
+        
         --if c1 == c2 
             --then putStrLn $ "we got two values" 
             --else putStrLn $ "nope"
-
     else do
         putStrLn $ " -- putting coin back in the box "
         putMVar box c2
         randomRIO (1,10) >>= \r -> threadDelay (r * 100000)
-        customerthreads cust b box
+        customerthreads cust mcusta xcoin box mcustb
 
 transfer :: Customer -> Customer -> Int -> IO (Customer, Customer)
 transfer from to amount
@@ -243,3 +263,9 @@ randomN :: IO Int
 randomN = do
     r <- randomRIO (10, 50)
     return r
+
+
+-- tomorrow - try different ways to get the customer threads to select each other
+-- first two threads that run?   
+
+-}
